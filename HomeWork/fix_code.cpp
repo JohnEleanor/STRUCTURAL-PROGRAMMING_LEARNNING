526776d58ec6b8658a06cfc61652d721301b6dbc
John Eleanor <mrs.ch64@gmail.com>
2023-08-02 17:11:01 +0700

    FIX CODE.cpp IS CORRECT 4 BUG IN THIS CODE

--- HomeWork/fix_code.cpp
+++ HomeWork/fix_code.cpp
@@ -2,14 +2,15 @@
 using namespace std; 
 int main() { 
     cout << "Welcome to my world!" << endl; 
-    const int pi = 3.14; 
+    double pi = 3.14; 
     cout << "Please enter 2 of your favorite integers: "; 
-    cin >> fav1, fav2; 
     short fav1, fav2; 
-    int 33a = fav1 + fav2;
+    cin >> fav1, fav2; 
+
+    short a33 = fav1 + fav2;
     float b33 = abs(float(fav1 - fav2)); 
     pi = 3.14159; 
-    cout << "You will get married at age: " << 33a << endl; 
+    cout << "You will get married at age: " << a33 << endl; 
     cout << "You will have " << b33 << " children" << endl; 
     cout << "Good luck! Bye ^^" << endl; return 0; 
 }
