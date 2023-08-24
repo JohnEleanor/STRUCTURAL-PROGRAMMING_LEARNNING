29368c1568d6e0b3eb7ffcbe67b21833048047aa
John Eleanor <mrs.ch64@gmail.com>
2023-08-25 01:08:21 +0700

    Plz enter five number

---
+++ HomeWork/enterfivenumber.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int __number[5];
+
+    int i; 
+
+    for ( i = 0; i < 5; i++)
+    {
+        cout << "plz enter for fivem number : ";
+        cin >> __number[i];
+    }
+
+    int counter;
+    cout  << "===========" << endl;
+    for (counter = 0; counter < sizeof(__number) / sizeof(__number[0]); counter++)
+    {
+
+        int num_1;
+        cout << __number[counter]<< " : ";
+        for (num_1 = 0; num_1 < __number[counter]; num_1++)
+        {
+            cout  << '*';
+        }
+        cout << endl;
+    }
+    cout <<  "===========" ;
+
+    
+
+    return 0;
+}
