e9bc748b388151c60040bba5e27dc2322c70cba7
John Eleanor <mrs.ch64@gmail.com>
2023-09-07 19:08:16 +0700

    this is real

--- HomeWork/FUNCTION/function_homework.cpp
+++ HomeWork/FUNCTION/function_homework.cpp
@@ -1,42 +1,17 @@
 
 #include <iostream>
+#include <string>
 using namespace std;
+string cal_grade(int _input_score_from_student);
+float input_score(int _input_score);
+string show_score_for_student(string _grade);
 
-/**
- @param _param_1 this is the score from input RQ:FUNCTION inputscore()
-*/
-
-float cal_grade(int _input_score_from_student)
-{
-    float _grade;
-    _grade = _input_score_from_student + 5.123;
-    return _grade;
-}
-
-/**
- @param _input_score this is the score from input RQ:input
-*/
-
-float input_score(int _input_score)
-{
-    float _convert_score = _input_score;
-    return _convert_score;
-}
-
-/**
- @param _grade this is the score from input RQ:input
-*/
-float show_score_for_student(float _grade)
-{
-    float _print_score = _grade;
-    return _print_score;
-}
 
 int main()
 {
     int score[20] = {};
     float convert_score[20] = {};
-    float calculate_score[20] = {};
+    string calculate_score[20] = {};
     int i = 1;
     while (i <= 20)
     {
@@ -54,4 +29,57 @@ int main()
     }
 
     return 0;
+}
+
+
+/**
+     @param _input_score_from_student this is the score from input RQ:FUNCTION inputscore()
+*/
+
+string cal_grade(int _input_score_from_student)
+{
+    int my_score = _input_score_from_student;
+    string _grade;
+
+    if (my_score >= 80 && my_score <= 100) {
+        _grade = "A";
+    }
+
+    if (my_score >= 60 && my_score <= 79) {
+        _grade = "B";
+    }
+
+    if (my_score >= 50 && my_score <= 69) {
+        _grade = "C";
+    }
+
+    if (my_score <= 49) {
+        _grade = "F";
+    }
+
+    if (_grade == ""){
+        _grade = "ERROR";
+    }
+
+
+    return _grade;
+}
+
+/**
+ @param _input_score this is the score from input RQ:input
+*/
+
+float input_score(int _input_score)
+{
+    float _convert_score = _input_score;
+    return _convert_score;
+}
+
+/**
+ @param _grade this is the score from input RQ:input
+*/
+string show_score_for_student(string _grade)
+{
+    string _print_score = _grade;
+    return _print_score;
 }
